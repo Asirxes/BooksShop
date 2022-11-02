2e728fae64a1b57ecee332bd6d79da15be58ee1d
Asirxes <karolkowalczykpraca0@gmail.com>
2022-11-02 15:02:16 +0100

    Created Repository

---
+++ Book.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Book.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Book.DataAccess;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; }
+}
